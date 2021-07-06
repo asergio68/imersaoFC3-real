70bd7b7ab21b5b9e749ddd256ab9bb28c32bc6f0
Antonio Sergio G Ribeiro <asergio68@gmail.com>
2021-07-06 02:42:48 -0300

    aula1

--- go-codebank/usecase/process_trasaction.go
+++ go-codebank/usecase/process_trasaction.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"time"
+
 	"github.com/asergio68/imersaoFC3-real/go-codebank/domain"
 	"github.com/asergio68/imersaoFC3-real/go-codebank/dto"
 )
@@ -14,5 +16,41 @@ func NewUseCaseTransaction(transactionRepository domain.TransactionRepository) U
 }
 
 func (u UseCaseTransaction) ProcessTransaction(transactionDto dto.Transaction) (domain.Transaction, error) {
-	
+	cc := u.HydrateCreditCard(transactionDto)
+	ccFull, err := u.TransactionRepository.GetCreditCard(*cc)
+	if err != nil{
+		return domain.Transaction{}, err
+	}
+	cc.Balance = ccFull.Balance
+	cc.Limit = ccFull.Limit
+	cc.ID = ccFull.ID
+	tr := u.NewTransaction(transactionDto, ccFull)
+	tr.ProcessAndValidate(cc)
+
+	err = u.TransactionRepository.SaveTransaction(*tr,*cc)
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+	return *tr, nil
+}
+
+func (u UseCaseTransaction) HydrateCreditCard(transactionDto dto.Transaction) *domain.CreditCard {
+	cc := domain.NewCreditCard()
+	cc.Name = transactionDto.Name
+	cc.Number = transactionDto.Number
+	cc.CVV = transactionDto.CVV
+	cc.ExpirationMonth = transactionDto.ExpirationMonth
+	cc.ExpirationYear = transactionDto.ExpirationYear
+	return cc
+}
+
+func (u UseCaseTransaction) NewTransaction(transactionDto dto.Transaction, cc domain.CreditCard) *domain.Transaction {
+	t := domain.NewTransaction()
+	t.ID = transactionDto.ID
+	t.CreditCardID = cc.ID
+	t.Amount = transactionDto.Amount
+	t.Description = transactionDto.Description
+	t.Store = transactionDto.Store
+	t.CreatedAt = time.Now()
+	return t
 }
